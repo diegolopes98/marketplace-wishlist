f5c82ab87abcfb8e7f31ccd47545cfd3158b704b
Diego Lopes <diegoplopes5@gmail.com>
2022-07-08 15:21:57 -0300

    feat: add swagger documentation

---
+++ src/main/java/com/marketplace/wishlist/infrastructure/configuration/SpringFoxConfiguration.java
@@ -0,0 +1,42 @@
+package com.marketplace.wishlist.infrastructure.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfiguration {
+
+    @Value("${docs.swagger.api.title}")
+    private String title;
+    @Value("${docs.swagger.api.description}")
+    private String description;
+    @Value("${docs.swagger.api.version}")
+    private String version;
+    @Value("${docs.swagger.api.base-package}")
+    private String basePackage;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(basePackage))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(title)
+                .description(description)
+                .version(version)
+                .build();
+    }
+}
