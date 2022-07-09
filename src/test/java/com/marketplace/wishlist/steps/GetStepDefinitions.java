4c60d10704217f8c49aecbe2a1c54c02705a86c9
Diego Lopes <diegoplopes5@gmail.com>
2022-07-09 18:19:02 -0300

    test: add basic tests to get

---
+++ src/test/java/com/marketplace/wishlist/steps/GetStepDefinitions.java
@@ -0,0 +1,48 @@
+package com.marketplace.wishlist.steps;
+
+import com.marketplace.wishlist.SpringIntegrationTest;
+import com.marketplace.wishlist.utils.HttpResponse;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+
+public class GetStepDefinitions extends SpringIntegrationTest {
+
+    private HttpResponse apiResponse;
+    private RestTemplate restTemplate;
+
+
+    public GetStepDefinitions(
+            @Qualifier("rest-template") RestTemplate restTemplate
+    ) {
+        this.restTemplate = restTemplate;
+        this.apiResponse = new HttpResponse();
+    }
+    @When("the API receive a get for wish {string} from customer id {string}")
+    public void theAPIReceiveAGetForWishFromCustomerId(String productId, String customerId) {
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(
+                    customerId + "/wishlist/" + productId,
+                    HttpMethod.GET,
+                    new HttpEntity<>(null),
+                    String.class
+            );
+            apiResponse.setStatus(response.getStatusCode());
+            apiResponse.setBody(response.getBody());
+        } catch (HttpStatusCodeException e) {
+            apiResponse.setStatus(e.getStatusCode());
+            apiResponse.setBody(e.getResponseBodyAsString());
+        }
+    }
+
+    @Then("the Get API must return {int}")
+    public void theGetAPIMustReturn(int statusCode) {
+        assert apiResponse.getStatus().equals(HttpStatus.resolve(statusCode));
+    }
+}
---
+++ src/test/resources/features/get.feature
@@ -0,0 +1,12 @@
+Feature: Get Specific Wish
+  Background:
+    Given an customer id "3880bae4-4552-451b-80c1-f972acd1199b" with a mock wishlist with the product ids
+      |57ab8693-b83e-409d-862b-bc4cb6ccab55|
+
+  Scenario: Get existent wish
+    When the API receive a get for wish "57ab8693-b83e-409d-862b-bc4cb6ccab55" from customer id "3880bae4-4552-451b-80c1-f972acd1199b"
+    Then the Get API must return 200
+
+  Scenario: Get non existent wish
+    When the API receive a get for wish "489217dd-8860-47d3-a2c6-625b27d88aed" from customer id "fc5d8209-cf41-4102-a851-81c715f2e921"
+    Then the Get API must return 404
